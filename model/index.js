b3dfbe763a3eaa6b86d474687ea071335e2735e9
Sergey-Markov <1nativefl@gmail.com>
2021-10-29 21:03:49 +0300

    add router.delete and operation removeContact, add createError at our routers, fix something with npm run lint:fix, deleted file service-api.js

--- model/index.js
+++ model/index.js
@@ -1,57 +1,60 @@
-const fs = require("fs/promises");
-const path = require("path");
-const crypto = require("crypto");
+const fs = require('fs/promises')
+const path = require('path')
+const crypto = require('crypto')
 
-const contactsPath = path.resolve(__dirname, "./contacts.json");
+const contactsPath = path.resolve(__dirname, './contacts.json')
 
 // const contacts = require('./contacts.json')
 
 const getContacts = async () => {
-  const contacts = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(contacts);
-};
+  const contacts = await fs.readFile(contactsPath, 'utf-8')
+  return JSON.parse(contacts)
+}
 
 const listContacts = async () => {
   try {
-    const contacts = await getContacts();
-    return contacts;
+    const contacts = await getContacts()
+    return contacts
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 const getContactById = async (contactId) => {
   try {
-    const contacts = await getContacts();
+    const contacts = await getContacts()
     const contact = contacts.find((contact) => {
       if (String(contact.id) === String(contactId)) {
-        return contact;
+        return contact
       } else {
-        return null;
+        return null
       }
-    });
-    return contact;
+    })
+    return contact
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 const removeContact = async (contactId) => {
   try {
-    const contacts = await getContacts();
+    const contacts = await getContacts()
     const contact = contacts.find((contact) => {
       if (String(contact.id) === String(contactId)) {
-        return contact;
+        return contact
+      } else {
+        return null
       }
-    });
-    const indexOfContact = contacts.indexOf(contact);
-    contacts.splice(indexOfContact, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts));
-    listContacts();
+    })
+    const indexOfContact = contacts.indexOf(contact)
+    contacts.splice(indexOfContact, 1)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts))
+    console.table(contacts)
+    return contact
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 const addContact = async ({ name, email, phone }) => {
   const newContact = {
@@ -59,38 +62,38 @@ const addContact = async ({ name, email, phone }) => {
     name,
     email,
     phone,
-  };
+  }
 
   try {
-    const contacts = await getContacts();
-    contacts.push(newContact);
-    const dataToJSON = JSON.stringify(contacts);
+    const contacts = await getContacts()
+    contacts.push(newContact)
+    const dataToJSON = JSON.stringify(contacts)
     await fs.writeFile(contactsPath, dataToJSON, (err) => {
-      if (err) throw err;
-    });
-    return newContact;
+      if (err) throw err
+    })
+    return newContact
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 const updateContact = async (contactId, body) => {
-  const contacts = await getContacts();
+  const contacts = await getContacts()
   const contactIndex = contacts.findIndex((contact) => {
-    if (Number(contact.id) === Number(contactId)) return contact;
-  });
+    if (Number(contact.id) === Number(contactId)) return contact
+  })
   if (contactIndex === -1) {
-    return null;
+    return null
   }
-  contacts[contactIndex] = { id: contactId, ...body };
+  contacts[contactIndex] = { id: contactId, ...body }
 
-  const dataToJSON = JSON.stringify(contacts);
+  const dataToJSON = JSON.stringify(contacts)
   await fs.writeFile(contactsPath, dataToJSON, (err) => {
-    if (err) throw err;
-  });
+    if (err) throw err
+  })
 
-  return contacts[contactIndex];
-};
+  return contacts[contactIndex]
+}
 
 module.exports = {
   listContacts,
@@ -98,4 +101,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-};
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,89 +1,101 @@
-const express = require("express");
-const contactsOperations = require("../../model");
-const router = express.Router();
-const createError = require("http-errors");
-const Joi = require("joi");
+const express = require('express')
+const contactsOperations = require('../../model')
+const router = express.Router()
+const createError = require('http-errors')
+const Joi = require('joi')
 
 const joiSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.number().required(),
-});
+})
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const listContacts = await contactsOperations.listContacts();
+    const listContacts = await contactsOperations.listContacts()
     res.json({
-      status: "success",
+      status: 'success',
       code: 200,
       data: listContacts,
-    });
+    })
   } catch (error) {
-    const err = new createError(500, `${error}`);
-    next(err);
+    const err = new createError(500, `${error}`)
+    next(err)
   }
-});
+})
 
-router.get("/:contactId", async (req, res, next) => {
+router.get('/:contactId', async (req, res, next) => {
   try {
-    const { contactId } = req.params;
-    const contact = await contactsOperations.getContactById(contactId);
+    const { contactId } = req.params
+    const contact = await contactsOperations.getContactById(contactId)
     if (!contact) {
       throw new createError(
         404,
         `Any Contact don't have id:${contactId}, contact not found`
-      );
+      )
     }
     res.json({
-      status: "success",
+      status: 'success',
       code: 200,
       data: contact,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const body = req.body;
-    const { error } = joiSchema.validate(body);
+    const body = req.body
+    const { error } = joiSchema.validate(body)
     if (error) {
-      throw new createError(400, error.message);
+      throw new createError(400, 'missing required name field')
     }
-    const newContact = await contactsOperations.addContact(body);
+    const newContact = await contactsOperations.addContact(body)
     res.status(201).json({
       status: 201,
       data: { ...newContact },
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.put("/:contactId", async (req, res, next) => {
+router.delete('/:contactId', async (req, res, next) => {
+  const { contactId } = req.params
   try {
-    const body = req.body;
-    const { error } = joiSchema.validate(body);
-    if (error) {
-      throw new createError(400, error.message);
+    const result = await contactsOperations.removeContact(contactId)
+    if (!result) {
+      throw new createError.NotFound(`Contact with id:${contactId} not found`)
     }
-    const { contactId } = req.params;
+    res.status(200).json({ message: 'contact deleted' })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    const body = req.body
+    const { error } = joiSchema.validate(body)
+    if (error) {
+      throw new createError(400, 'missing fields')
+    }
+    const { contactId } = req.params
     const updatedContact = await contactsOperations.updateContact(
       contactId,
       body
-    );
+    )
+    if (!updatedContact) {
+      throw new createError.NotFound(`Contact with id:${contactId} not found`)
+    }
     res.json({
       status: 200,
-      data: { updatedContact },
-    });
+      data: { ...updatedContact },
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
