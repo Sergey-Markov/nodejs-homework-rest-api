58b0b353cca0e4b16d3384359719fff89eeb5f72
Sergey-Markov <1nativefl@gmail.com>
2021-11-14 21:46:50 +0200

    add to dir models - User change rout of auth create function registration

--- controllers/auth/registration.js
+++ controllers/auth/registration.js
@@ -1,3 +1,18 @@
-const registration = (req, res) => {};
+const { User } = require("../../models");
+const { Conflict } = require("http-errors");
+
+const registration = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict(`User with email:${email} allready exist`);
+  }
+  await User.create({ email, password });
+  res.status(201).json({
+    status: "Successful",
+    code: 201,
+    message: "Registration successful",
+  });
+};
 
 module.exports = registration;
