dcf5b783886efbb729fbafe82dc5b413d66c5304
Sergey-Markov <1nativefl@gmail.com>
2021-11-15 22:28:30 +0200

    add pagination in ctrl.getContactsList

--- controllers/contacts/getContactsList.js
+++ controllers/contacts/getContactsList.js
@@ -1,8 +1,14 @@
 const { Contact } = require("../../models");
 
-const getContactsList = async (_, res) => {
+const getContactsList = async (req, res) => {
+  const { page, limit } = req.query;
+  const skip = (page - 1) * limit;
   const { _id } = req.user;
-  const listContacts = await Contact.find({ owner: _id });
+  const listContacts = await Contact.find(
+    { owner: _id },
+    "_id name email phone owner",
+    { skip, limit: +limit }
+  ).populate("owner", "_id email");
   res.json({
     status: "success",
     code: 200,
