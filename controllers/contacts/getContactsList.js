ebc3794bbe17ff4fd5d5696d01885c7a2964d159
Sergey-Markov <1nativefl@gmail.com>
2021-11-15 21:24:19 +0200

    change logic in ctrl.getContactsList add method find({owner:_id})

--- controllers/contacts/getContactsList.js
+++ controllers/contacts/getContactsList.js
@@ -1,7 +1,8 @@
 const { Contact } = require("../../models");
 
 const getContactsList = async (_, res) => {
-  const listContacts = await Contact.find();
+  const { _id } = req.user;
+  const listContacts = await Contact.find({ owner: _id });
   res.json({
     status: "success",
     code: 200,
