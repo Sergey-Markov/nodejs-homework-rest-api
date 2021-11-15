9f55a3d01cbb03d2d7c2efe9d23302de186cb16f
Sergey-Markov <1nativefl@gmail.com>
2021-11-15 14:45:18 +0200

    add JWT and complete ctrl.login

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,6 +1,9 @@
 const { User } = require("../../models");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { NotFound, Unauthorized } = require("http-errors");
 const bcrypt = require("bcryptjs");
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -12,8 +15,20 @@ const login = async (req, res) => {
   if (!comparePasswordsResults) {
     throw new Unauthorized("Email or Password is wrong, try again");
   }
+  const payload = {
+    id: user._id,
+  };
+  console.log(payload);
+  console.log(SECRET_KEY);
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
   res.json({
-    message: "wellcome",
+    status: "Success",
+    code: 200,
+    message: "Wellcome",
+    data: {
+      token,
+    },
   });
 };
 
