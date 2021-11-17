4919906a9e3a84e6b3f51b1f29baa998db2a7d09
Sergey-Markov <1nativefl@gmail.com>
2021-11-17 20:10:50 +0200

    add logic in ctrl.getContactsList - get listOfFavoriteContacts depending on query

--- controllers/contacts/getContactsList.js
+++ controllers/contacts/getContactsList.js
@@ -1,19 +1,34 @@
 const { Contact } = require("../../models");
 
 const getContactsList = async (req, res) => {
-  const { page, limit } = req.query;
-  const skip = (page - 1) * limit;
+  const { page, limit, favorite } = req.query;
   const { _id } = req.user;
-  const listContacts = await Contact.find(
-    { owner: _id },
-    "_id name email phone owner",
-    { skip, limit: +limit }
-  ).populate("owner", "_id email");
-  res.json({
-    status: "success",
-    code: 200,
-    data: listContacts,
-  });
+  if (page && limit) {
+    const skip = (page - 1) * limit;
+    const listContacts = await Contact.find(
+      { owner: _id },
+      "_id name email phone favorite owner",
+      { skip, limit: +limit }
+    ).populate("owner", "_id email");
+    res.json({
+      status: "success",
+      code: 200,
+      data: listContacts,
+    });
+  }
+
+  if (favorite) {
+    const listOfFavoriteContacts = await Contact.find({
+      owner: _id,
+      favorite: favorite,
+    });
+    console.log(listOfFavoriteContacts);
+    res.status(200).json({
+      status: "Succes",
+      code: 200,
+      data: listOfFavoriteContacts,
+    });
+  }
 };
 
 module.exports = getContactsList;
