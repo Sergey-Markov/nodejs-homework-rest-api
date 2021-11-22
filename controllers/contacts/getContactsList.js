2abbbfe77449582e1eab3ff159634604a4e49879
Sergey-Markov <1nativefl@gmail.com>
2021-11-22 12:04:34 +0200

    Для favorite есть дефолтное значение при создании контакта - false выполнено - на параметры page и limit сделать проверку на число, если не число - вернуть ошибку badRequest

--- controllers/contacts/getContactsList.js
+++ controllers/contacts/getContactsList.js
@@ -2,32 +2,41 @@ const { Contact } = require("../../models");
 
 const getContactsList = async (req, res) => {
   const { page = 1, limit = 100, favorite } = req.query;
-  const { _id } = req.user;
-  if (page && limit) {
-    const skip = (page - 1) * limit;
-    const listContacts = await Contact.find(
-      { owner: _id },
-      "_id name email phone favorite owner",
-      { skip, limit: +limit }
-    ).populate("owner", "_id email");
-    res.json({
-      status: "success",
-      code: 200,
-      data: listContacts,
-    });
-  }
+  if (page >= 1 && limit >= 1) {
+    const { _id } = req.user;
+    if (page && limit) {
+      const skip = (page - 1) * limit;
+      const listContacts = await Contact.find(
+        { owner: _id },
+        "_id name email phone favorite owner",
+        { skip, limit: +limit }
+      ).populate("owner", "_id email");
+      res.json({
+        status: "success",
+        code: 200,
+        data: listContacts,
+      });
+    }
 
-  if (favorite) {
-    const listOfFavoriteContacts = await Contact.find({
-      owner: _id,
-      favorite: favorite,
+    if (favorite) {
+      const listOfFavoriteContacts = await Contact.find({
+        owner: _id,
+        favorite: favorite,
+      });
+      console.log(listOfFavoriteContacts);
+      res.status(200).json({
+        status: "Succes",
+        code: 200,
+        data: listOfFavoriteContacts,
+      });
+    }
+  } else {
+    res.status(400).json({
+      status: "BadRequest",
+      code: 400,
+      message: "field page or limit has incorrect type",
     });
-    console.log(listOfFavoriteContacts);
-    res.status(200).json({
-      status: "Succes",
-      code: 200,
-      data: listOfFavoriteContacts,
-    });
+    return;
   }
 };
 
