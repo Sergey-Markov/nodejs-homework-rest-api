f8ca8b5bcefca13b5fab4b447065172e95e2d373
Sergey-Markov <1nativefl@gmail.com>
2021-12-01 13:03:28 +0200

    add res and err messages

--- controllers/auth/verify.js
+++ controllers/auth/verify.js
@@ -5,11 +5,15 @@ const verify = async (req, res) => {
   const { verificationToken } = req.params;
   const user = await User.findOne({ verificationToken });
   if (!user) {
-    throw new NotFound();
+    throw new NotFound("User not found");
   }
   await User.findByIdAndUpdate(user._id, {
     verificationToken: null,
     verify: true,
+  });
+  res.status(200).json({
+    status: "200 Ok",
+    message: "Verification successful",
   });
 };
 
