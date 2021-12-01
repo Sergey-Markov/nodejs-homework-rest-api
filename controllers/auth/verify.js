241fdacfa0170ca32d5a3e435e18e5dcfc7af265
Sergey-Markov <1nativefl@gmail.com>
2021-12-01 11:45:26 +0200

    create ctrl.verify

---
+++ controllers/auth/verify.js
@@ -0,0 +1,16 @@
+const { NotFound } = require("http-errors");
+const { User } = require("../../models");
+
+const verify = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw new NotFound();
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verificationToken: null,
+    verify: true,
+  });
+};
+
+module.exports = verify;
