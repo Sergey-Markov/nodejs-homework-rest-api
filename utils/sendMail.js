4be4577cf0f8e8fdbf607cb98b81e213c6ddf873
Sergey-Markov <1nativefl@gmail.com>
2021-12-01 11:46:06 +0200

    create utils sendMail

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,18 +3,6 @@ require("dotenv").config();
 const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-// const mail = {
-//   to: "",
-//   from: "1nativeflag@gmail.com",
-//   subject: "Подтверждение регистрации",
-//   html: `<a href="http//:localhost:3000/users//verify/${verificationToken}"> Нажмите для подтверждения</a> `,
-// };
-
-// sgMail
-//   .send(mail)
-//   .then(() => console.log("Email success send"))
-//   .catch((error) => console.log(error.message));
-
 const sendMail = async (data) => {
   const email = { ...data, from: "1nativeflag@gmail.com" };
   await sgMail.send(email);
